e1140425f2114111c1d024ea4450e20737b6709c
szymon-michal <245852@edu.p.lodz.pl>
2022-10-20 17:08:18 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+    printf("Podaj pierwsza przyprostokatna:");
+    scanf("%f", &a);
+    printf("Podaj druga przyprostokatna :");
+    scanf("%f", &b);
+    c = sqrt(a*a + b*b);
+    printf("Przeciwprostokatna:%.2f",c);
+}
